95f56034f6968a56b66f0426586e9a390ef34a0f
Khalil <khalilpati55@gmail.com>
2024-04-22 19:02:42 +0530

    Getting Started

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "~/styles/globals.css";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Next Gallary",
+  description: "An app built using Next, RSC, Drizzle, PostgreSQL and Tailwind",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${inter.variable}`}>{children}</body>
+    </html>
+  );
+}
