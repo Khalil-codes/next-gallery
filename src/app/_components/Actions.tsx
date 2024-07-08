3d311c402951ed9f652d6813fd727eaae558e39e
Khalil <khalilpati55@gmail.com>
2024-07-08 10:02:55 +0530

    Add tooltip to provide info on hover

--- src/app/_components/Actions.tsx
+++ src/app/_components/Actions.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { DownloadIcon, HeartIcon } from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { ShareIcon } from "lucide-react";
 import React from "react";
 
@@ -12,22 +18,36 @@ const Actions = () => {
         Download
       </Button>
       <div className="flex items-center gap-2">
-        <Button
-          variant="ghost"
-          size="icon"
-          className="text-muted-foreground dark:text-muted-foreground"
-        >
-          <HeartIcon className="h-4 w-4" />
-          <span className="sr-only">Like</span>
-        </Button>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="text-muted-foreground dark:text-muted-foreground"
-        >
-          <ShareIcon className="h-4 w-4" />
-          <span className="sr-only">Share</span>
-        </Button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="text-muted-foreground dark:text-muted-foreground"
+              >
+                <HeartIcon className="h-4 w-4" />
+                <span className="sr-only">Like</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Like</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="text-muted-foreground dark:text-muted-foreground"
+              >
+                <ShareIcon className="h-4 w-4" />
+                <span className="sr-only">Share</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Share</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </>
   );
